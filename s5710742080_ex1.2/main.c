35ac912ea65121107a23653730e7524880739786
hathaichanok <hathai_milk@hotmail.com>
2014-09-02 17:22:38 +0700

    Hathaichanok

---
+++ s5710742080_ex1.2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    scanf("%d %d %d",&a, &b, &c);
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+    printf("c = %d\n",c);
+    system("pause");
+    return 0;
+}
